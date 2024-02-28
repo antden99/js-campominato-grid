06084fafc1aeedf57dd4fb4b043e204c70542c1d
antden99 <antoniodenigris@icloud.com>
2024-02-28 15:57:56 +0100

    add loop for in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 console.log("tutto funziona");
+
+// mi seleziono il conteiner all'interno del cocumento e lo stampo in console
+const containerEl = document.querySelector(".container");
+console.log(containerEl);
+
+//creo un elemento ul e all'interno 
+const ul = document.createElement("ul");
+
+//appendo la lista al container contenuto nel documento
+containerEl.append(ul);
+console.log(ul);
+
+//creo un ciclo for, così da appendere all'interno della lista le mie 100 celle
+for (let i = 1; i <= 100; i++) {
+    let elementList = document.createElement("div");
+    ul.append(elementList);
+
+    elementList.insertAdjacentHTML("beforeend", i);
+}
