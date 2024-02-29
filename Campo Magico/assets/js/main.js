5811410b7ba2f293d02a3c37b15f1fd204ee2dbf
antden99 <antoniodenigris@icloud.com>
2024-02-29 12:15:51 +0100

    add folder called Campo Magico
    add the random number calculate

--- Campo Magico/assets/js/main.js
+++ Campo Magico/assets/js/main.js
@@ -24,7 +24,7 @@ clickButton.addEventListener("click", function () {
     ul.innerHTML = " ";
 
     let difficult = 0; // inizializzo una variabile a cui assegnerò successivamente il numero di iterazioni
-    
+
     const userChoice = document.querySelector("select");
     let choice = userChoice.value
     console.log(choice);
@@ -42,42 +42,45 @@ clickButton.addEventListener("click", function () {
         containerEl.style.width = "1000px";
         console.log(difficult)
     }
-        //creo un ciclo for, così da appendere all'interno della lista le mie 100 celle
+    //creo un ciclo for, così da appendere all'interno della lista le mie 100 celle
 
-        for (let i = 1; i <= difficult; i++) {
-            let elementList = document.createElement("div");
-            ul.append(elementList);
+    for (let i = 1; i <= difficult; i++) {
+        let elementList = document.createElement("div");
+        ul.append(elementList);
 
-            elementList.innerHTML = i;
-        }
-        // accedo adesso a tutti i nodi della ul, cosi che al click ne cambi il colore(creando una funzione Listner)
-        const allNode = document.querySelectorAll("ul > div");
-        //console.log(allNode);
+        elementList.innerHTML = i;
+    }
+    // accedo adesso a tutti i nodi della ul, cosi che al click ne cambi il colore(creando una funzione Listner)
+    const allNode = document.querySelectorAll("ul > div");
+    //console.log(allNode);
 
-        for (let i = 0; i < allNode.length; i++) {
-            const element = allNode[i];
-            //console.log(element);
+    for (let i = 0; i < allNode.length; i++) {
+        const element = allNode[i];
+        //console.log(element);
 
-            element.addEventListener("click", function () {
+        element.addEventListener("click", function () {
 
-                element.classList.toggle("cyan");
-                console.log(`hai cliccato la cella numero: ${i + 1}`)//scrivo +1 per il linguaggio umano
+            element.classList.toggle("cyan");
+            console.log(`hai cliccato la cella numero: ${i + 1}`)//scrivo +1 per il linguaggio umano
 
-            })
-        }
+        })
+    }
 
-    })
-
-
+})
 
 
+//scrivo il codice per creare 16 numeri casuali da 1 a 16 e li pusho in un array vuoto creato appositamente
+const listRandomNumb = [];
+
+//finche la lunghezza dell'array non raggiunge 16 elementi allora continua a ciclare
+while (listRandomNumb.length != 16) {
+    let elementNumbRandom = (Math.floor(Math.random() * 100) + 1);
+    console.log(elementNumbRandom);
+    if (!listRandomNumb.includes(elementNumbRandom)) {
+        listRandomNumb.push(elementNumbRandom);
+    }
+}
 
 
 
-
-
-
-
-
-
-
+console.log(listRandomNumb);
