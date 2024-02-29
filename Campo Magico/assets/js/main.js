db50c02098b92de3a42d26036506019adc1d2895
antden99 <antoniodenigris@icloud.com>
2024-02-29 12:23:58 +0100

    add loop and control

--- Campo Magico/assets/js/main.js
+++ Campo Magico/assets/js/main.js
@@ -42,13 +42,35 @@ clickButton.addEventListener("click", function () {
         containerEl.style.width = "1000px";
         console.log(difficult)
     }
+
+    //scrivo il codice per creare 16 numeri casuali da 1 a 16 e li pusho in un array vuoto creato appositamente
+    const listRandomNumb = [];
+
+    //finche la lunghezza dell'array non raggiunge 16 elementi allora continua a ciclare
+    while (listRandomNumb.length != 16) {
+        let elementNumbRandom = (Math.floor(Math.random() * difficult) + 1);
+        console.log(elementNumbRandom);
+        if (!listRandomNumb.includes(elementNumbRandom)) {
+            listRandomNumb.push(elementNumbRandom);
+        }
+    }
+
+    //mi stampo in console la lista di numeri generata per controllare che sia corretta
+    console.log(listRandomNumb);
+
+
     //creo un ciclo for, così da appendere all'interno della lista le mie 100 celle
 
     for (let i = 1; i <= difficult; i++) {
         let elementList = document.createElement("div");
         ul.append(elementList);
 
-        elementList.innerHTML = i;
+        if (listRandomNumb.includes(i)) {
+             elementList.innerHTML = `<i class="fa-solid fa-bomb"></i>`; 
+        } else {
+            elementList.innerHTML = i;
+        }
+
     }
     // accedo adesso a tutti i nodi della ul, cosi che al click ne cambi il colore(creando una funzione Listner)
     const allNode = document.querySelectorAll("ul > div");
@@ -69,18 +91,4 @@ clickButton.addEventListener("click", function () {
 })
 
 
-//scrivo il codice per creare 16 numeri casuali da 1 a 16 e li pusho in un array vuoto creato appositamente
-const listRandomNumb = [];
 
-//finche la lunghezza dell'array non raggiunge 16 elementi allora continua a ciclare
-while (listRandomNumb.length != 16) {
-    let elementNumbRandom = (Math.floor(Math.random() * 100) + 1);
-    console.log(elementNumbRandom);
-    if (!listRandomNumb.includes(elementNumbRandom)) {
-        listRandomNumb.push(elementNumbRandom);
-    }
-}
-
-
-
-console.log(listRandomNumb);
